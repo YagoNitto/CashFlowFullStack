4caeca56bf47b8efc62af5353f635ab6687e3526
Yago <yago_nitto@hotmail.com>
2024-06-19 21:35:04 -0300

    first commit

---
+++ Dima.Web/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace Dima.Web;
+
+public static class Configuration
+{
+    public const string HttpClientName = "dima";
+    public static string BackendUrl { get; set; } = "http://localhost:5112";
+    
+    public static MudTheme Theme = new()
+    {
+        Typography = new Typography
+        {
+            Default = new Default
+            {
+                FontFamily = ["Raleway", "sans-serif"]
+            }
+        },
+        Palette = new PaletteLight
+        {
+            Primary = "#1EFA2D",
+            Secondary = Colors.LightGreen.Darken3,
+            Background = Colors.Grey.Lighten4,
+            AppbarBackground = "#1EFA2D",
+            AppbarText = Colors.Shades.Black,
+            TextPrimary = Colors.Shades.Black,
+            PrimaryContrastText = new MudColor("#000000"),
+            DrawerText = Colors.Shades.White,
+            DrawerBackground = Colors.LightGreen.Darken4
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = Colors.LightGreen.Accent3,
+            Secondary = Colors.LightGreen.Darken3,
+            AppbarBackground = Colors.LightGreen.Accent3,
+            AppbarText = Colors.Shades.Black,
+            PrimaryContrastText = new MudColor("#000000")
+        }
+    };
+}
