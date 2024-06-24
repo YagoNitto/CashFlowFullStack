bb5dd5a65baed7d565f8478e70d58166283eade5
Yago <yago_nitto@hotmail.com>
2024-06-24 00:34:42 -0300

    Finish transaction CRUD

--- Dima.Api/Handlers/TransactionHandler.cs
+++ Dima.Api/Handlers/TransactionHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Dima.Api.Data;
 using Dima.Core.Common.Extensions;
+using Dima.Core.Enums;
 using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Transactions;
@@ -12,6 +13,9 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
 {
     public async Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
     {
+        if (request is { Type: ETransactionType.Withdraw, Amount: >= 0 })
+            request.Amount *= -1;
+        
         try
         {
             var transaction = new Transaction
@@ -38,6 +42,9 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
 
     public async Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
     {
+        if (request is { Type: ETransactionType.Withdraw, Amount: >= 0 })
+            request.Amount *= -1;
+        
         try
         {
             var transaction = await context.Transactions
@@ -117,10 +124,10 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
         try
         {
             var query = context.Transactions.AsNoTracking()
-                .Where(x => x.CreatedAt >= request.StartDate &&
-                            x.CreatedAt <= request.EndDate &&
+                .Where(x => x.PaidOrReceivedAt >= request.StartDate &&
+                            x.PaidOrReceivedAt <= request.EndDate &&
                             x.UserId == request.UserId)
-                .OrderBy(x => x.CreatedAt);
+                .OrderBy(x => x.PaidOrReceivedAt);
 
             var transactions = await query
                 .Skip((request.PageNumber - 1) * request.PageSize)
--- Dima.Core/Requests/Transactions/CreateTransactionRequest.cs
+++ Dima.Core/Requests/Transactions/CreateTransactionRequest.cs
@@ -7,8 +7,9 @@ public class CreateTransactionRequest : Request
 {
     [Required(ErrorMessage = "Título inválido")]
     public string Title { get; set; } = string.Empty;
+
     [Required(ErrorMessage = "Tipo inválido")]
-    public ETransactionType Type { get; set; }
+    public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
     [Required(ErrorMessage = "Valor inválido")]
     public decimal Amount { get; set; }
     [Required(ErrorMessage = "Categoria inválida")]
